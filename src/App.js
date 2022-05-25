fe1088cc10f0eeb933f608f6866175ef1fe1c71b
Nick Chen <nickchenchj@gmail.com>
2022-05-25 19:30:14 +0800

    My first React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>To 小寶,</h1>
+        <p>I love you more than anything in this world. You are my 歐姆蛋咖哩豬排飯!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
