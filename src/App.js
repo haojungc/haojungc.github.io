be1784ee10ba187247138acd55ace86cad559eba
nick <haojungc@gmail.com>
2022-06-03 13:52:51 +0800

    Initialize react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>To 小寶,</h1>
-        <p>I love you more than anything in this world. You are my 歐姆蛋咖哩豬排飯!</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
