d72efcfe6be250c435c8e0c338d1c7f03354215e
Hao-Jung Chen <haojungc@gmail.com>
2022-11-19 00:42:30 -0800

    Add table of exercises

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableOfExercisesComponent } from './table-of-exercises/table-of-exercises.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'oLxBG9ki.html', component: TableOfExercisesComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
